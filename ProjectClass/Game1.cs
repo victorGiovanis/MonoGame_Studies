cfce538a5fcf6de8de1052dc9b19c5cd35b94989
Victor <victorgiovanni769@gmail.com>
2024-04-15 16:03:31 -0300

    Added project

--- ProjectClass/Game1.cs
+++ ProjectClass/Game1.cs
@@ -8,7 +8,13 @@ namespace ProjectClass
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Color _bgColor;
+        private double _time;
+
+        // Temporizador
         private SpriteFont _font;
+        private string _timeText;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,6 +27,9 @@ namespace ProjectClass
             // TODO: Add your initialization logic here
 
             base.Initialize();
+
+            _bgColor = Color.Black;
+            _time = 0.0;
         }
 
         protected override void LoadContent()
@@ -37,16 +46,32 @@ namespace ProjectClass
 
             // TODO: Add your update logic here
 
+            _timeText = string.Format("Tempo: {0}", gameTime.TotalGameTime.TotalSeconds);
+
+            _time = _time + gameTime.ElapsedGameTime.TotalSeconds;
+            if (_time > 2.0)
+            {
+                _time = 0.0;
+                if (_bgColor == Color.White)
+                {
+                    _bgColor = Color.Black;
+                }
+                else
+                {
+                    _bgColor = Color.White;
+                }
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(_bgColor);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(_font, "Texto Exemplo", Vector2.Zero, Color.Black);
+            _spriteBatch.DrawString(_font, _timeText, Vector2.Zero, Color.Purple);
             _spriteBatch.End();
 
             base.Draw(gameTime);
--- ProjectClass/obj/Debug/net6.0/ProjectClass.AssemblyInfo.cs
+++ ProjectClass/obj/Debug/net6.0/ProjectClass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3c4547e9dc9f2dd537a658db46608127102f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
